a3a2eaaece87cc31b39b8dd69048dbe0fda30fa4
Wey Wen <38684095+wwenlim@users.noreply.github.com>
2022-11-28 21:10:55 +0800

    Update dates.R

--- dates.R
+++ dates.R
@@ -1,2 +1,5 @@
-# 1. Getting the first date of every month
-seq(from = as.Date(${1:list}), to = ceiling_date(Sys.Date(), "month") - 1, by = "days")
+# 1. Simple formats
+as.Date("2022-11-28") # ISO-8601 format works best
+
+# 2. Getting the first date of every month
+seq(from = as.Date(${1:start_date}), to = as.Date(${2:end_date}), by = ${3:list})) # 3: "days"/"months"/"years"
